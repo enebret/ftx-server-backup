4e540955847e87764e05cac2f5d80d87157d710b
Ikenna <enebret@yahoo.com>
2023-07-21 13:25:02 +0100

    added the database script and directory

---
+++ DbConnect.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const mongoose = require ('mongoose');
+const mongoURI = process.env.mongoURI;
+const connectDb = async () => {
+    try {
+        mongoose.set('strictQuery', true);
+        await mongoose.connect(mongoURI, {useNewUrlParser: true,});
+        console.log('connected to mongoatlas database server...')
+    } catch (error) {
+        console.log(error)
+    }
+};
+connectDb()
